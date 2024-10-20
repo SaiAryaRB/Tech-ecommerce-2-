f9aaed5035fa79aba8f63c7dc724a36497e715f5
SaiAryaRB <saiaryarb2003@gmail.com>
2024-10-20 18:32:25 +0530

    Cart Logic 2

--- SalesManagement/Backend/routes/cartRoutes.js
+++ SalesManagement/Backend/routes/cartRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { cart } = require('../controllers/cartController'); // Adjust the path
-
+const {cartinfo} =require('../controllers/cartViewController') ;
 // Route to add a product to the cart
 router.post('/cart', cart);
+router.post('/viewcart',cartinfo)
 
 module.exports = router;
